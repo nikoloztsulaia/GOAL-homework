e5626adaf8a03720b61a896e1c3111594d26600a
nikoloztsulaia <nikolozi.wulaia@gmail.com>
2025-03-21 00:24:21 +0400

    commit

---
+++ day 06/homework/lesson5.py
@@ -0,0 +1,23 @@
+# მომხმარებელს შემოატანინეთ საყვარელი სპორტი  და ტერმინალში დაბეჭდეთ ის
+favorite_sport = input("your favorite sport:")
+print(favorite_sport)
+# მომხმარებელს შეეკითხეთ სახელი, გვარი, ასაკი, საცხოვრებელი ქალაქი,ჰობი შემდეგ კი დაბეჭდეთ გრძელი წინადადება მაგ. შენი სახელია "სახელი და გვარი" შენი ასაკია "ასაკი" და ასე შემდეგ.
+name = input("tape your name:")
+surname = input("enter your surname:")
+age = input("entert your age:")
+home = input("enter where are you live:")
+hobby = input("enter your hobby:")
+print("your name is"  + name +  " "  + surname +  "you are"  + age +  "years old you live in"  + home +  "and your hobby is" + hobby + "")
+# მოხმარებელს შემოატანინეთ რიცხვი და მაგ რიცხვს გამოაკელით 20 და გაამრავლეთ 3 ზე
+num = int (input("enter num:"))
+print(num + 20 * 3 )
+# მოხმარებელს შემოატანინეთ რიცხვები ანუ num,num1,num2...და ასე შემდეგ და საბოლოოდ გამოითვალეთ მაგ რიცხების საშუალო არითმეტიკულიმაგალიტად: 2, 2, 2 = 2 + 2 + 2 = 6 / 3 = 2 ანუ რიცხვების ჯამი გავყოთ რაოდენობაზე(მინიმუმ 3 რიცხვის ცვლადი გქონდეთ შექმნილი
+num1 = int (input("enter num1:"))
+num2 = int (input("enter num2:"))
+num3 = int (input("enter num3:"))
+num4 = int (input("enter num4:"))
+total = num1 + num2 + num3 + num4
+print(total / 4)
+
+
+

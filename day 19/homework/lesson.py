e47e445fb29c4c40d6e5ade6f71046bfc0de3cc5
nikoloztsulaia <nikolozi.wulaia@gmail.com>
2025-07-01 17:31:24 +0400

    commit

---
+++ day 19/homework/lesson.py
@@ -0,0 +1,38 @@
+# numbers = [10, 20, 30, 40, 50, 60, 70]
+# აიღე ამ სიიდან პირველი 3 ელემენტი slicing-ის გამოყენებით.
+
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+first_three = numbers[0:3]
+
+print(first_three)
+
+
+
+# letters = ['a', 'b', 'c', 'd', 'e', 'f']
+# დაპრინტე სიიდან ელემენტები 'c', 'd', 'e' slicing-ით.
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+print(letters[2:5])
+
+
+# words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+# აიღე სიის ბოლო 2 ელემენტი slicing-ის მეშვეობით.
+
+words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
+
+print(words[-2:])
+
+
+# colors = ['red', 'green', 'blue', 'yellow', 'purple']
+# დაპრინტე სია შუიდან დაწყებული ბოლომდე (სადღაც 'blue'-დან ბოლომდე) slicing-ით.
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+
+print(colors[2:])
+
+
+# text = "programming"
+# დაპრინტე მხოლოდ პირველი 5 სიმბოლო slicing-ის მეშვეობით.
+text = "programming"
+
+print(text[0:5])
